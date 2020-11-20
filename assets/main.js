c1848dd43048e6f96c4b235d4d4a910891032206
Ale <alessandrochb@gmail.com>
2020-11-20 14:51:52 +0100

    stampata in pagina prima email (la mail arriva in ritardo)

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,23 @@
+// scaricare libreria axios
 var app = new Vue ({
     el: '#root',
     data:{
-
+        email: false,
     },
     methods:{
 
     },
     mounted(){
         console.log('ciao');
+        const self = this;
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(elemento){
+                console.log(elemento);
+                console.log(elemento.data);
+                console.log(elemento.data.response);
+                console.log(self.email);
+                self.email = elemento.data.response;
+            })
     },
 
 
